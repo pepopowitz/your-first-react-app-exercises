f301ae13e3297f9fac1789a76f8055b2309cb423
Steven Hicks <steven.j.hicks@gmail.com>
2018-08-17 22:54:52 -0500

    initial commit - a boilerplate for react modules with a shared node_modules

---
+++ module-1/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  rootDir: '../',
+  collectCoverageFrom: ['<rootDir>/module-1/**/*.{js,jsx,mjs}'],
+  setupFiles: ['<rootDir>/_not_important/config/polyfills.js'],
+  testMatch: [
+    '<rootDir>/module-1/**/__tests__/**/*.{js,jsx,mjs}',
+    '<rootDir>/module-1/**/?(*.)(spec|test).{js,jsx,mjs}',
+  ],
+  testEnvironment: 'node',
+  testURL: 'http://localhost',
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/_not_important/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|css|json)$)':
+      '<rootDir>/_not_important/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+    'mjs',
+  ],
+};
