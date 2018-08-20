7c2301c28865f05bbcdc7f3e4c54aa71fbe85125
Steven Hicks <steven.j.hicks@gmail.com>
2018-08-20 17:34:11 -0500

    Update add.spec.js

---
+++ module-0/exercise-1/add.spec.js
@@ -0,0 +1,17 @@
+// 1. Uncomment lines 2-8 to test the `add` function
+// describe('add', () => {
+//   it('adds two numbers', () => {
+//     const result = add(1, 4); 
+    
+//     expect(result).toEqual(5);
+//   });
+// });
+
+// 2. Uncomment lines 11-17 to test the `addThree` function
+// describe('addThree', () => {
+//   it('adds 3 to something', () => {
+//     const result = addThree(5); 
+    
+//     expect(result).toEqual(8);
+//   });
+// });
