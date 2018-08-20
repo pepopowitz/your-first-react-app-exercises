9181431b49fa5a491468df19869ac815923280c9
Steven Hicks <steven.j.hicks@gmail.com>
2018-08-20 17:35:42 -0500

    Update greetings.js

---
+++ module-0/exercise-3/greetings.js
@@ -0,0 +1,15 @@
+
+export function buildGreeting(name) {
+}
+
+export function choosePartingWord(language) {
+}
+
+export function choosePartingWordDestructured({name, language}) {
+}
+
+export function mapPartingWords(languages) {
+}
+
+export function removeUnknownLanguages(languages) {
+}
