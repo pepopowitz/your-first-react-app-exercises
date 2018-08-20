9d7de03c013bd04de23248093ad76c75ff2441ae
Steven Hicks <steven.j.hicks@gmail.com>
2018-08-20 17:34:48 -0500

    Update things.js

---
+++ module-0/exercise-2/things.js
@@ -0,0 +1,13 @@
+//  a class in ES is not like you'd think of a class in C# or Java.
+//  It is "syntactical sugar" - this class gets turned into a normal ES object.
+//  We won't talk much about the differences between this and C#/Java,
+//   for now just know that this thing will mostly act like you'd expect
+//   a class to act in your code.
+
+export class ThingOne {
+
+}
+
+export class ThingTwo {
+  
+}
