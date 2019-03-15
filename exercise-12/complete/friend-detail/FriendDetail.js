a671c5269c758ab2b44c72e1b1b50d2293eb4134
Steven Hicks <steven.j.hicks@gmail.com>
2019-03-14 21:44:38 -0500

    Some missed updates from previous exercise rewrites

--- exercise-12/complete/friend-detail/FriendDetail.js
+++ exercise-12/complete/friend-detail/FriendDetail.js
@@ -6,19 +6,6 @@ import FriendFlipper from './FriendFlipper';
 
 import styles from './FriendDetail.module.css';
 
-function renderFriend(friend) {
-  if (friend === undefined) {
-    return <h1>Loading...</h1>;
-  }
-  
-  return (
-    <div className={styles.cardContents}>
-      <h1>{friend.name}</h1>
-      <FriendFlipper friend={friend} />
-      <p>{friend.bio}</p>
-    </div>
-  );
-}
 export default function({ friend }) {
   return (
     <Page>
@@ -29,5 +16,19 @@ export default function({ friend }) {
         <Card>{renderFriend(friend)}</Card>
       </div>
     </Page>
+  );
+}
+
+function renderFriend(friend) {
+  if (friend === undefined) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className={styles.cardContents}>
+      <h1>{friend.name}</h1>
+      <FriendFlipper friend={friend} />
+      <p>{friend.bio}</p>
+    </div>
   );
 }
